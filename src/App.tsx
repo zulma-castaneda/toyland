316b3f7c6287c600597401b19d88965e22d3f0be
Zulma Castañeda <57265842+zulma-castaneda@users.noreply.github.com>
2024-05-03 10:24:17 -0500

    Map adjustments (#1)
    * linting
    
    * Modified map and added copy for islands
    
    * Fixed scroll issue after selecting an island
    
    * Improved scroll hierarchy
    
    ---------
    
    Co-authored-by: Zulma Castaneda <zulma.castaneda@endava.com>
    Co-authored-by: Diego Vasquez <davasqueza@unal.edu.co>

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import './App.css'
-import { Outlet } from 'react-router-dom';
+import "./App.css";
+import { Outlet } from "react-router-dom";
 
 export function App() {
   return (
-    <div className='app-container'>
-      <div className='header'>
+    <div className="app-container">
+      <div className="header">
         <b>Super header</b>
       </div>
-      <div className='route-container'>
+      <div className="route-container">
         <Outlet />
       </div>
-      <div className='footer'>
+      <div className="footer">
         <b>Super footer</b>
       </div>
     </div>
-  )
+  );
 }
